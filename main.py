779e8fc5305304c8e56d33d9ddd4d5cfbfec9b7f
f1yToMoon <st102441@student.spbu.ru>
2025-11-30 17:42:56 +0300

    second task

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from flask import Flask, request, jsonify
+import json
+import time
 import os
+
+CONFIG_PATH = "config.json"
+with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+    config = json.load(f)
+
+FILE_PATH = config["current_state_path"]
+SNAPSHOT_FILE = config["snapshot_path"]
+SNAPSHOT_INTERVAL = config["snapshot_interval_sec"]
+
+LAST_SNAPSHOT_TIME = 0
 
 app = Flask(__name__)
 
-FILE_PATH = "data.txt"
-
 @app.put("/replace")
 def replace():
-    data = request.get_json()
+    global LAST_SNAPSHOT_TIME
 
+    data = request.get_json()
     if not data or "source" not in data or "id" not in data or "payload" not in data:
         return jsonify({"error": "invalid json"}), 400
 
+    payload = data["payload"]
+
     with open(FILE_PATH, "w", encoding="utf-8") as f:
-        f.write(data["payload"])
+        f.write(payload)
+
+    now = time.time()
+    if now - LAST_SNAPSHOT_TIME >= SNAPSHOT_INTERVAL:
+        with open(SNAPSHOT_FILE, "a", encoding="utf-8") as f:
+            f.write(payload + "\n")
+        LAST_SNAPSHOT_TIME = now
 
     return jsonify({"status": "ok"}), 200
 
